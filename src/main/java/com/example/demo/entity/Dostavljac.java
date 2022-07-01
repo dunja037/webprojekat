6a8e74e2e2b69da73c8b15742c3fcb5169d84619
dunja037 <106025908+dunja037@users.noreply.github.com>
2022-07-01 23:42:19 +0200

    Update Dostavljac.java

--- src/main/java/com/example/demo/entity/Dostavljac.java
+++ src/main/java/com/example/demo/entity/Dostavljac.java
@@ -1,45 +1,111 @@
 package com.example.demo.entity;
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.*;
+import java.io.Serializable;
+import javax.persistence.*;
+
+;
 
 @Entity
-public class Dostavljac extends Korisnik implements Serializable {
+public class Porudzbina implements Serializable {
+    @Id
+    @GeneratedValue
+    private UUID uuid;
 
-    @OneToMany
-    @JoinColumn(name = "dostavljacId")
-    private Set<Porudzbina> porudzbine = new HashSet<>();
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<ArtikalZaPorudzbinu> artikalzaporudzbinu = new HashSet<>();
 
-    public Dostavljac() {
-        this.uloga = Uloga.DOSTAVLJAC;
+    @ManyToOne
+    @JoinColumn(name = "restoranIdLokacija")
+    @JsonIgnore
+    private Restoran restoran;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column
+    private Date datum;
+
+    @Column
+    private float cena;
+
+    @ManyToOne
+    @JoinColumn(name = "korisnickoIme")
+    private Kupac kupac;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    Status status;
+
+
+    public Porudzbina() {
     }
 
-    //Registration mapping
-    public Dostavljac(Korisnik korisnik){
-        this.uloga = Uloga.DOSTAVLJAC;
-        this.ime = korisnik.ime;
-        this.prezime = korisnik.prezime;
-        this.korisnicko = korisnik.korisnicko;
-        this.datum_rodjenja = korisnik.datum_rodjenja;
-        this.pol = korisnik.pol;
-        this.lozinka = korisnik.lozinka;
+    public Porudzbina(UUID uuid, Set<ArtikalZaPorudzbinu> artikalzaporudzbinu, Restoran restoran, Date datum, float cena, Kupac kupac, Status status) {
+        this.uuid = uuid;
+        this.artikalzaporudzbinu = artikalzaporudzbinu;
+        this.restoran = restoran;
+        this.datum = datum;
+        this.cena = cena;
+        this.kupac = kupac;
+        this.status = status;
     }
 
-    public Dostavljac(String korisnicko_ime, String lozinka, String ime, String prezime, Pol pol, Date datum_rodjenja, Set<Porudzbina> porudzbine) {
-        super(korisnicko_ime, lozinka, ime, prezime, pol, datum_rodjenja);
-        this.porudzbine = porudzbine;
-        this.uloga = Uloga.DOSTAVLJAC;
+
+    public UUID getUuid() {
+        return uuid;
     }
 
-    public Set<Porudzbina> getPorudzbine() {
-        return porudzbine;
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
     }
 
-    public void setPorudzbine(Set<Porudzbina> porudzbine) {
-        this.porudzbine = porudzbine;
+    public Set<ArtikalZaPorudzbinu> getStavke() {
+        return artikalzaporudzbinu;
+    }
+
+    public void setArtikalzaporudzbinu(Set<ArtikalZaPorudzbinu> artikalzaporudzbinu) {
+        this.artikalzaporudzbinu = artikalzaporudzbinu;
+    }
+
+    public Restoran getRestoran() {
+        return restoran;
+    }
+
+    public void setRestoran(Restoran restoran) {
+        this.restoran = restoran;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+
+    public float getCena() {
+        return cena;
+    }
+
+    public void setCena(float cena) {
+        this.cena = cena;
+    }
+
+    public Kupac getKupac() {
+        return kupac;
+    }
+
+    public void setKupac(Kupac kupac) {
+        this.kupac = kupac;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
